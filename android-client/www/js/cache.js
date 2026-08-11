d45142c1b0776ca361d63456442a566ff6f068b4
xincheng <xincheng213618@gmail.com>
2026-08-11 21:09:15 +0800

    fix(android): unify integrated cache version chain

--- android-client/www/js/cache.js
+++ android-client/www/js/cache.js
@@ -1,4 +1,4 @@
-import { CLIENT_VERSION } from "./config.js?v=20260811-favorite-folders-02";
+import { CLIENT_VERSION } from "./config.js?v=20260812-android-integrated-01";
 
 const DB_NAME = "fanhao-android-cache";
 const DB_VERSION = 2;
